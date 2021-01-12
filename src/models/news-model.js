0773bc9425c8cfe4f4356f699afd95c9737ef813
Martin Rodriguez <martin.rdfg@gmail.com>
2021-01-12 17:08:13 -0300

    news model revised

--- src/models/news-model.js
+++ src/models/news-model.js
@@ -3,13 +3,13 @@ const mongoosePaginate = require('mongoose-paginate-v2');
 const { Schema } = mongoose;
 const NewsSchema = new Schema({
     publish_date: { type: Date },
-    title: { type: String },
+    title: { type: String, required: true, unique: true },
     subtitle: { type: String },
     category: { type: String },
     description: { type: String },
     author: { type: String },
-    link: { type: String },
-    feed: { type: Schema.Types.ObjectId, ref: 'feed' }
+    link: { type: String, required: true },
+    feed: { type: Schema.Types.ObjectId, ref: 'feed', required: true }
 }, { timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' } });
 
 NewsSchema.plugin(mongoosePaginate);
